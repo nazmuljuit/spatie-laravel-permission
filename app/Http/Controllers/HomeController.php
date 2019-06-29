95c159c4f7bdd366816837731b1192026a477a53
Nazmul Hossain <otteaninc@gmail.com>
2019-06-29 20:29:57 +0600

    some change

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -28,11 +29,29 @@ class HomeController extends Controller
         //$role = Role::create(['name' => 'writer']);
         //$permission = Permission::create(['name' => 'edit articles']);
         //$permission = Permission::create(['name' => 'write articles']);
-        $role = Role::findById(1);
-        $permission = Permission::findById(1);
+        //$role = Role::findById(1);
+        //$permission = Permission::findById(1);
         //$role->givePermissionTo($permission);
         //$permission->removeRole($role);
-        $role->revokePermissionTo($permission);
+        //$role->revokePermissionTo($permission);
         return view('home');
+    }
+
+    public function test()
+    {
+        //$role = Role::create(['name' => 'operator']);
+        //$permission = Permission::create(['name' => 'data entry']);
+        //auth()->user()->assignRole('operator');
+        //auth()->user()->givePermissionTo('data entry');
+        //$role = Role::findById(2);
+        //$permission = Permission::findById(3);
+        //$role->givePermissionTo($permission);
+        //return auth()->user()->permissions;
+        //return auth()->user()->getDirectPermissions();
+        //return auth()->user()->getAllPermissions();
+        //return User::role('operator')->get();
+        //return User::permission('data entry')->get();
+        //auth()->user()->revokePermissionTo('data entry');
+        //auth()->user()->removeRole('operator');
     }
 }
